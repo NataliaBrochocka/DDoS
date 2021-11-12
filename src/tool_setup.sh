527b295b0f917f31338f18f30f1b90868cd22a6f
jokuniewska <julia.okuniewska1@gmail.com>
2021-11-13 00:58:34 +0100

    Optimize tool installation on VMs
    Move downloading go&telegraf from VM to Host. Files are downloaded only once, not every VM setup

--- src/tool_setup.sh
+++ src/tool_setup.sh
@@ -4,15 +4,14 @@ sudo apt-get update
 sudo apt-get install -y make
 sudo apt-get install -y python3-venv
 
-echo "Download and install golang"
+echo "Installing golang"
 
-wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /home/vagrant/tmp_src/go1.17.linux-amd64.tar.gz
 echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile
 
 
-echo "Download and install telegraf"
+echo "Installing telegraf"
 
-wget https://dl.influxdata.com/telegraf/releases/telegraf_1.20.0~rc0-1_amd64.deb
-sudo dpkg -i telegraf_1.20.0~rc0-1_amd64.deb
+sudo dpkg -i /home/vagrant/tmp_src/telegraf_1.20.0~rc0-1_amd64.deb

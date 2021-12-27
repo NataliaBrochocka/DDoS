f584bf9ba5a0309327ebf9d93e9f7f10962021cc
mstachow97 <92484737+mstachow97@users.noreply.github.com>
2021-12-27 17:36:03 +0100

    Fix scapy not available for python script

--- src/tool_setup.sh
+++ src/tool_setup.sh
@@ -3,11 +3,11 @@ echo "Update packages and install make, python3-venv, scapy, tshark"
 sudo apt-get update
 sudo apt-get install -y make
 sudo apt-get install -y python3-venv
-sudo apt-get install -y scapy
+sudo apt-get install -y python3-pip
+sudo pip3 install scapy
 
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
-
 
 echo "Installing golang"
 
@@ -19,4 +19,3 @@ echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile
 echo "Installing telegraf"
 
 sudo dpkg -i /home/vagrant/tmp_src/telegraf_1.20.0~rc0-1_amd64.deb
-
